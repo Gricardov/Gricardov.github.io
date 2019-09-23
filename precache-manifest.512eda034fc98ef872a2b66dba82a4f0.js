7123430640c6256df475b782ab88406c930d1f54
gricardov <gricardov@gmail.com>
2019-09-23 01:00:09 -0500

    despliegue prueba temple luna v1.0

---
+++ precache-manifest.512eda034fc98ef872a2b66dba82a4f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e4a1ff46addccb3f53206c6c9786c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65cc81e0747463223fd5",
+    "url": "/static/css/main.0fb658da.chunk.css"
+  },
+  {
+    "revision": "f8f58b38af4e6ba55ced",
+    "url": "/static/js/2.344f708f.chunk.js"
+  },
+  {
+    "revision": "65cc81e0747463223fd5",
+    "url": "/static/js/main.af0096e1.chunk.js"
+  },
+  {
+    "revision": "c8f9e809e21db8bf2a1f",
+    "url": "/static/js/runtime~main.7214b1fa.js"
+  }
+]);
